78ce5f208b40911c60ce0c2855d128347e93090a
moreauf <moreauf@NGT15.theobald-groupe.local>
2021-01-28 16:31:18 +0100

    debug

--- core/modules/societe/doc/pdf_compte_client.modules.php
+++ core/modules/societe/doc/pdf_compte_client.modules.php
@@ -255,13 +255,32 @@ class pdf_compte_client
   				$out = $outputlangs->convToOutputCharset($object->idprof4);
   				$pdf->MultiCell(120, 0, $out,0,'L');
   				
-  				$object->fetchObjectLinked();
-  				var_dump($object->linkedObjects);
-  				exit;
-  				if(!empty($object->linkedObjects['operationorder'])){
-  					
-
+  				
+  				
+  				$sql = "SELECT t.lastname, t.firstname, t.civility, t.poste";
+  				$sql .= " FROM ".MAIN_DB_PREFIX."socpeople as t";
+  				$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "societe_contacts as c on t.rowid = c.fk_socpeople";
+  				$sql .= " WHERE t.fk_soc = ".$object->id . " AND c.fk_c_type_contact = 1031113";
+  				
+  				$resql=$this->db->query($sql);
+  				if($resql){
+  				    
+  				    $obj = $db->fetch_object($resql);
+  				    
+  				    $out = $obj->lastname. " ";
+  				    $out .= $obj->firstname;
+  				    
+  				    $pdf->SetFont('','', $default_font_size+2);
+  				    $pdf->SetXY(90, 115);
+  				    $out = $outputlangs->convToOutputCharset($out);
+  				    $pdf->MultiCell(120, 0, $out,0,'L');
+  				    
+  				    $pdf->SetFont('','', $default_font_size+2);
+  				    $pdf->SetXY(90, 123);
+  				    $out = $outputlangs->convToOutputCharset($obj->poste);
+  				    $pdf->MultiCell(120, 0, $out,0,'L');
   				}
+  				
   				
 				$pdf->Close();
 
