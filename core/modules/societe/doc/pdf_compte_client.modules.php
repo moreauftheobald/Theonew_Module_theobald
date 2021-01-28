11ca3e09bae0acfdd1690c95b9394e22735bab85
moreauf <moreauf@NGT15.theobald-groupe.local>
2021-01-28 17:16:08 +0100

    debug

--- core/modules/societe/doc/pdf_compte_client.modules.php
+++ core/modules/societe/doc/pdf_compte_client.modules.php
@@ -208,7 +208,7 @@ class pdf_compte_client
                 
 				$pdf->SetFont('','', $default_font_size+2);
 				$pdf->SetXY(140, 20);
-				//$out = $extrafields->showOutputField("options_site",$object->array_options["options_site"]);
+				$out = $extrafields->showOutputField("options_site",$object->array_options["options_site"]);
 				$out = $outputlangs->convToOutputCharset($out);
 				$pdf->MultiCell(50, 0, $out,0,'R');
 				
